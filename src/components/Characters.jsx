55cb88fffbc1f00c3c8d977ee5e1f96dc2e62b90
jordicher <jordi_sodiac@hotmail.com>
2022-01-16 16:58:12 +0100

    add hook useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,23 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react';
 
-const Characters = () => {
-
-    const [characters, setCharacters] = useState([])
-
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character/').then(res => res.json()).then(data => setCharacters(data.results))
-
-    }, [])
-
-
-    return (
-        <div className='characters'>
-            {characters.map(character => (
-                <h2>{character.name}</h2>
-            ))}
-
-        </div>
-    )
+const initialState = {
+    favorites: []
 }
 
-export default Characters
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            };
+        default:
+            return state;
+    }
+}
+
+const Characters = () => {
+    const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character/')
+            .then(response => response.json())
+            .then(data => setCharacters(data.results));
+    }, []);
+
+    const handleClick = favorite => {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
+    return (
+        <div className="Characters">
+
+            {favorites.favorites.map(favorite => (
+                <li key={favorite.id}>
+                    {favorite.name}
+                </li>
+            ))}
+
+            {characters.map(character => (
+                <div className="item" key={character.id}>
+                    <h2>{character.name}</h2>
+                    <button type="button" onClick={() => handleClick(character)}>Agregar a Favoritos</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Characters;
